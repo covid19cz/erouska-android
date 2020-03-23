7e21143fabd65bb5cf3a62986a5f7dd0368288e8
Tomáš Chlápek <tochlape@microsoft.com>
2020-03-23 22:08:39 +0100

    UI fixes
    Toolbar up and back button hidden from help fragment when it is shown with bottom bar
    Floating button added for Bluetooth Disabled fragment

--- app/src/main/kotlin/cz/covid19cz/app/ui/help/HelpFragment.kt
+++ app/src/main/kotlin/cz/covid19cz/app/ui/help/HelpFragment.kt
@@ -6,6 +6,7 @@ import cz.covid19cz.app.R
 import cz.covid19cz.app.databinding.FragmentHelpBinding
 import cz.covid19cz.app.ui.base.BaseFragment
 import cz.covid19cz.app.ui.help.event.HelpCommandEvent
+import kotlinx.android.synthetic.main.fragment_help.welcome_continue_btn
 
 class HelpFragment : BaseFragment<FragmentHelpBinding, HelpVM>(R.layout.fragment_help, HelpVM::class) {
 
@@ -13,7 +14,7 @@ class HelpFragment : BaseFragment<FragmentHelpBinding, HelpVM>(R.layout.fragment
         super.onCreate(savedInstanceState)
 
         subscribe(HelpCommandEvent::class) {
-            when(it.command){
+            when (it.command) {
                 HelpCommandEvent.Command.GO_BACK -> goBack()
             }
         }
@@ -21,10 +22,16 @@ class HelpFragment : BaseFragment<FragmentHelpBinding, HelpVM>(R.layout.fragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        enableUpInToolbar(true, IconType.CLOSE)
+        enableUpInToolbar(arguments?.getBoolean("fullscreen") == true, IconType.CLOSE)
+
+        if(arguments?.getBoolean("fullscreen") == true){
+            welcome_continue_btn.visibility = View.VISIBLE
+        } else {
+            welcome_continue_btn.visibility = View.GONE
+        }
     }
 
-    fun goBack(){
+    fun goBack() {
         navController().navigateUp()
     }
 }
