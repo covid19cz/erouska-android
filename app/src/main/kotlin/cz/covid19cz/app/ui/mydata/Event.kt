81fa28e78a320682ee552abf733cbce694a665f2
Jakub Beránek <berykubik@gmail.com>
2020-03-27 13:45:54 +0100

    Change design of My Data screen header

--- app/src/main/kotlin/cz/covid19cz/app/ui/mydata/Event.kt
+++ app/src/main/kotlin/cz/covid19cz/app/ui/mydata/Event.kt
@@ -3,7 +3,8 @@ package cz.covid19cz.app.ui.mydata
 import arch.event.LiveEvent
 
 sealed class ExportEvent : LiveEvent() {
-
     data class PleaseWait(val minutes: Int): ExportEvent()
     object Confirmation: ExportEvent()
 }
+
+object ShowDescriptionEvent: LiveEvent()
