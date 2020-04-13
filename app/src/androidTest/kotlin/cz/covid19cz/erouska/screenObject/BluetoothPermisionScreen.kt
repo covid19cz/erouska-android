4aea0059666eb6387d851e7f10070a75fb58a3c2
Michal Kubele <michal.kubele@gmail.com>
2020-04-13 14:05:06 +0200

    Add disableAnimations test rule

--- app/src/androidTest/kotlin/cz/covid19cz/erouska/screenObject/BluetoothPermisionScreen.kt
+++ app/src/androidTest/kotlin/cz/covid19cz/erouska/screenObject/BluetoothPermisionScreen.kt
@@ -1,13 +1,11 @@
 package cz.covid19cz.erouska.screenObject
 
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.UiObject
 import androidx.test.uiautomator.UiSelector
 import cz.covid19cz.erouska.R
 
@@ -17,9 +15,9 @@ class BluetoothPermissionScreen {
         onView(withId(R.id.enable_bluetooth_btn)).perform(click())
         // Initialize UiDevice instance
         val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
-        val cancelButton = device.findObject(
+        val allowButton = device.findObject(
             UiSelector().resourceId("com.android.packageinstaller:id/permission_allow_button")
         )
-        cancelButton.click()
+        allowButton.click()
     }
 }
