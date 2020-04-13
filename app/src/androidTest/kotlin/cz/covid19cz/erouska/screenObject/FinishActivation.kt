b9150be537728d70f3d1203d7c980fda98b888a2
Radim Daniel Panek <rdpanek@gmail.cz>
2020-04-13 23:43:57 +0200

    screenObject/FinishActivation.kt: fix waitFor resource-id

--- app/src/androidTest/kotlin/cz/covid19cz/erouska/screenObject/FinishActivation.kt
+++ app/src/androidTest/kotlin/cz/covid19cz/erouska/screenObject/FinishActivation.kt
@@ -1,9 +1,14 @@
 package cz.covid19cz.erouska.screenObject
 
+
+
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import cz.covid19cz.erouska.R
+import org.awaitility.Awaitility.await
+import java.util.concurrent.TimeUnit
 
 class FinishActivation {
     companion object {
@@ -11,11 +16,10 @@ class FinishActivation {
     }
 
     fun finish() {
-    /*
-        await().dontCatchUncaughtExceptions().atLeast(5, TimeUnit.SECONDS).untilAsserted {
-            onView(withId(R.id.battery_opt_title)).check(matches(isDisplayed()))
+
+        await().ignoreExceptions().atMost(5, TimeUnit.SECONDS).untilAsserted {
+            onView(withId(R.id.battery_opt_title)).check(matches(withText(TITLE)))
         }
-    */
 
         onView(withId(R.id.done_btn)).perform(click())
     }
