42687e98a88102be8e72273d7ab85f2d29b02a5d
Adam Simek <simekadam@gmail.com>
2020-04-13 07:52:30 +0200

    Update unregistration copy for unverified numbers + add new error state to login
    Users can't retroactively verify a number that has been already retroactively verified on another device.
    Also when deleting account with unverified number it doesn't delete all the accounts "tied" to that number.

--- app/src/main/kotlin/cz/covid19cz/erouska/ui/confirm/DeleteUserFragment.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/ui/confirm/DeleteUserFragment.kt
@@ -10,7 +10,13 @@ import cz.covid19cz.erouska.utils.Auth
 import cz.covid19cz.erouska.utils.formatPhoneNumber
 
 class DeleteUserFragment : ConfirmationFragment() {
-    override val description by lazy { getString(R.string.delete_user_desc, Auth.getPhoneNumber().formatPhoneNumber())}
+    override val description by lazy {
+        if (Auth.isPhoneNumberVerified()) {
+            getString(R.string.delete_user_desc, Auth.getPhoneNumber().formatPhoneNumber())
+        } else {
+            getString(R.string.delete_user_desc_unverified)
+        }
+    }
     override val buttonTextRes = R.string.delete_registration
     override fun confirmedClicked() {
         viewModel.deleteUser()
