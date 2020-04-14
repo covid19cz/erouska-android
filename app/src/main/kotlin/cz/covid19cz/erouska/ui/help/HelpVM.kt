da451f60a664e6522f8027058947cac39adc120b
David Vávra <david@vavra.me>
2020-04-14 11:02:23 +0200

    Release fixes
    - links are localized
    - verify later dissapears when there is error
    - keyboard dissapears when you verify later
    - two missing texts localized

--- app/src/main/kotlin/cz/covid19cz/erouska/ui/help/HelpVM.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/ui/help/HelpVM.kt
@@ -1,6 +1,5 @@
 package cz.covid19cz.erouska.ui.help
 
-import cz.covid19cz.erouska.AppConfig
 import cz.covid19cz.erouska.ui.base.BaseVM
 import cz.covid19cz.erouska.ui.help.event.HelpCommandEvent
 
@@ -8,9 +7,5 @@ class HelpVM : BaseVM() {
 
     fun goBack() {
         publish(HelpCommandEvent(HelpCommandEvent.Command.GO_BACK))
-    }
-
-    fun getTutorialUrl(): String {
-        return AppConfig.tutorialLink
     }
 }
