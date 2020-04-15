537a06188dfea24fca0f71c6fdcf43b7b42034ff
Adam Simek <simekadam@gmail.com>
2020-04-15 11:30:27 +0200

    Add autolink extension to markdown parser
    Some links are placed directly in the text and for various reasons can't be markdowned 🙂

--- app/src/main/kotlin/cz/covid19cz/erouska/utils/Markdown.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/utils/Markdown.kt
@@ -2,13 +2,21 @@ package cz.covid19cz.erouska.utils
 
 import android.content.Context
 import android.widget.TextView
+import io.noties.markwon.AbstractMarkwonPlugin
 import io.noties.markwon.Markwon
 import io.noties.markwon.image.glide.GlideImagesPlugin
+import org.commonmark.ext.autolink.AutolinkExtension
+import org.commonmark.parser.Parser
 
 class Markdown(val context: Context) {
     private val markwon by lazy {
         Markwon.builder(context)
             .usePlugin(GlideImagesPlugin.create(context))
+            .usePlugin(object: AbstractMarkwonPlugin() {
+                override fun configureParser(builder: Parser.Builder) {
+                    builder.extensions(listOf(AutolinkExtension.create()))
+                }
+            })
             .build()
     }
 
