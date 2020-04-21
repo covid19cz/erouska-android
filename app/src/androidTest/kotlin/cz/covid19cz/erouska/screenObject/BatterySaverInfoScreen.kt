c66075b895e50d7da06ed7a90a3c252ad50ec874
JanTrnavsky <jan.trnavsky96@gmail.com>
2020-04-21 16:58:50 +0200

    skip battery saver screen for devices without it

--- app/src/androidTest/kotlin/cz/covid19cz/erouska/screenObject/BatterySaverInfoScreen.kt
+++ app/src/androidTest/kotlin/cz/covid19cz/erouska/screenObject/BatterySaverInfoScreen.kt
@@ -3,6 +3,7 @@ package cz.covid19cz.erouska.screenObject
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.*
 import cz.covid19cz.erouska.R
+import cz.covid19cz.erouska.helpers.ManufacturerHelper
 import org.awaitility.Awaitility.await
 import java.util.concurrent.TimeUnit
 
@@ -10,6 +11,9 @@ object BatterySaverInfoScreen {
     const val TITLE = "eRouška potřebuje běžet i když s ní právě nepracujete"
 
     fun finish() {
+
+        // skip this screen for devices that does not require it
+        if(!ManufacturerHelper.isBatteryTutorialNeeded()) return
 
         await().ignoreExceptions().atMost(15, TimeUnit.SECONDS).untilAsserted {
             onView(withId(R.id.battery_opt_title)).checkMatchesString(TITLE)
