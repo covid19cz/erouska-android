8bcd09c9e1564ea1610de93a380fccde975810eb
JanTrnavsky <jan.trnavsky96@gmail.com>
2020-04-22 17:21:39 +0200

    create Action for UI Automator click

--- app/src/androidTest/kotlin/cz/covid19cz/erouska/helpers/Actions.kt
+++ app/src/androidTest/kotlin/cz/covid19cz/erouska/helpers/Actions.kt
@@ -6,11 +6,18 @@ import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiSelector
 import org.hamcrest.CoreMatchers.containsString
+
+private val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
 fun ViewInteraction.click(): ViewInteraction = perform(ViewActions.click())
 
 fun click(id: Int): ViewInteraction =  onView(withId(id)).click()
+
+fun clickUiAutomator(id: String) = device.findObject(UiSelector().resourceId(id)).click()
 
 fun checkMatchesString(id: Int, @StringRes stringId: String): ViewInteraction = onView(withId(id)).check(
     ViewAssertions.matches(withText(stringId))
--- app/src/androidTest/kotlin/cz/covid19cz/erouska/screenObject/BluetoothPermisionScreen.kt
+++ app/src/androidTest/kotlin/cz/covid19cz/erouska/screenObject/BluetoothPermisionScreen.kt
@@ -1,28 +1,21 @@
 package cz.covid19cz.erouska.screenObject
 
 import android.os.Build
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.uiautomator.UiDevice
-import androidx.test.uiautomator.UiSelector
 import cz.covid19cz.erouska.R
 import cz.covid19cz.erouska.helpers.checkDisplayed
 import cz.covid19cz.erouska.helpers.click
+import cz.covid19cz.erouska.helpers.clickUiAutomator
 
 object BluetoothPermissionScreen {
-    private val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
     fun allowPermission() {
         checkDisplayed(R.id.bluetooth_onboard_title)
         click(R.id.enable_bluetooth_btn)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            val allowLocation = findUiObject("com.android.permissioncontroller:id/permission_allow_foreground_only_button")
-            allowLocation.click()
+            clickUiAutomator("com.android.permissioncontroller:id/permission_allow_foreground_only_button")
         } else {
-            val allowButton = findUiObject("com.android.packageinstaller:id/permission_allow_button")
-            allowButton.click()
+            clickUiAutomator("com.android.packageinstaller:id/permission_allow_button")
         }
     }
-
-    private fun findUiObject(resourceId: String) = device.findObject(UiSelector().resourceId(resourceId))
 }
