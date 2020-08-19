564b05d4c78ab5ec6f9b5aa12cebb5008c4ddfdf
Pavel <pavel.habzansky@eman.cz>
2020-08-19 16:03:47 +0200

    Item prevention placeholders

---
+++ app/src/main/kotlin/cz/covid19cz/erouska/ui/exposure/PreventionAdapter.kt
@@ -0,0 +1,48 @@
+package cz.covid19cz.erouska.ui.exposure
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import cz.covid19cz.erouska.R
+import cz.covid19cz.erouska.databinding.ItemPreventionBinding
+import cz.covid19cz.erouska.ui.exposure.entity.PreventionItem
+
+class PreventionAdapter(
+    private var items: List<PreventionItem> = emptyList()
+) : RecyclerView.Adapter<PreventionAdapter.PreventionViewHolder>() {
+
+    fun setItems(newItems: List<PreventionItem>) {
+        items = newItems
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PreventionViewHolder {
+        val binding = DataBindingUtil.inflate<ItemPreventionBinding>(
+            LayoutInflater.from(parent.context),
+            R.layout.item_prevention,
+            parent,
+            false
+        )
+        return PreventionViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PreventionViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    inner class PreventionViewHolder(private val binding: ItemPreventionBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: PreventionItem) {
+            binding.symptomLabel.text = item.label
+            Glide.with(binding.root.context).load(item.iconUrl)
+                .placeholder(R.drawable.ic_item_empty).into(binding.symptomImg)
+        }
+
+    }
+
+}
--- app/src/main/kotlin/cz/covid19cz/erouska/ui/exposure/SpreadPreventionFragment.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/ui/exposure/SpreadPreventionFragment.kt
@@ -7,6 +7,7 @@ import cz.covid19cz.erouska.AppConfig
 import cz.covid19cz.erouska.R
 import cz.covid19cz.erouska.databinding.FragmentSpreadPreventionBinding
 import cz.covid19cz.erouska.ui.base.BaseFragment
+import cz.covid19cz.erouska.ui.exposure.entity.PreventionData
 import cz.covid19cz.erouska.ui.exposure.event.PreventionEvent
 import kotlinx.android.synthetic.main.fragment_spread_prevention.*
 
@@ -14,6 +15,10 @@ class SpreadPreventionFragment : BaseFragment<FragmentSpreadPreventionBinding, S
     R.layout.fragment_spread_prevention,
     SpreadPreventionVM::class
 ) {
+
+    private val adapter: PreventionAdapter by lazy {
+        PreventionAdapter()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +34,15 @@ class SpreadPreventionFragment : BaseFragment<FragmentSpreadPreventionBinding, S
 
     private fun updateState(state: PreventionEvent) {
         when (state) {
-            is PreventionEvent.PreventionDataLoaded -> prevention_text.text = state.data.title
+            is PreventionEvent.PreventionDataLoaded -> setData(state.data)
         }
+    }
+
+    private fun setData(data: PreventionData) {
+        prevention_text.text = data.title
+        prevention_list.adapter = adapter
+        prevention_list.setHasFixedSize(true)
+        adapter.setItems(data.items)
     }
 
 }
