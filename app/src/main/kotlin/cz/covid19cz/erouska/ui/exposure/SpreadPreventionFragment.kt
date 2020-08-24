90cde39ad1f09503a3ca23cd34f3b07f308b088e
Pavel-Habzansky <habzansky.pavel@gmail.com>
2020-08-24 14:11:53 +0200

    Feature/item placeholders (#249)
    * Item prevention placeholders
    
    * Symptoms placeholders
    
    * Symptom/prevention adapter removal
    
    * Redundant layout strategy removal
    
    Co-authored-by: Pavel <pavel.habzansky@eman.cz>

--- app/src/main/kotlin/cz/covid19cz/erouska/ui/exposure/SpreadPreventionFragment.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/ui/exposure/SpreadPreventionFragment.kt
@@ -2,35 +2,20 @@ package cz.covid19cz.erouska.ui.exposure
 
 import android.os.Bundle
 import android.view.View
-import androidx.lifecycle.observe
 import cz.covid19cz.erouska.AppConfig
 import cz.covid19cz.erouska.R
 import cz.covid19cz.erouska.databinding.FragmentSpreadPreventionBinding
 import cz.covid19cz.erouska.ui.base.BaseFragment
-import cz.covid19cz.erouska.ui.exposure.event.PreventionEvent
-import kotlinx.android.synthetic.main.fragment_spread_prevention.*
 
 class SpreadPreventionFragment : BaseFragment<FragmentSpreadPreventionBinding, SpreadPreventionVM>(
     R.layout.fragment_spread_prevention,
     SpreadPreventionVM::class
 ) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        viewModel.state.observe(this) { updateState(it) }
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         activity?.title = AppConfig.spreadPreventionUITitle
-    }
-
-    private fun updateState(state: PreventionEvent) {
-        when (state) {
-            is PreventionEvent.PreventionDataLoaded -> prevention_text.text = state.data.title
-        }
     }
 
 }
