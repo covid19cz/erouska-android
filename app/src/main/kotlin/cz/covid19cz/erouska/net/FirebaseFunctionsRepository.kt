f4e4bbd803034ae00d6bad92fd0926946bc4ac90
Tomáš Chlápek <tochlape@microsoft.com>
2020-09-01 14:59:14 +0200

    Firebase Function definition.

--- app/src/main/kotlin/cz/covid19cz/erouska/net/FirebaseFunctionsRepository.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/net/FirebaseFunctionsRepository.kt
@@ -7,14 +7,19 @@ import cz.covid19cz.erouska.AppConfig.FIREBASE_REGION
 import cz.covid19cz.erouska.db.SharedPrefsRepository
 import cz.covid19cz.erouska.net.model.CovidStatsResponse
 import cz.covid19cz.erouska.utils.DeviceInfo
+import cz.covid19cz.erouska.utils.L
 import cz.covid19cz.erouska.utils.LocaleUtils
 import kotlinx.coroutines.tasks.await
+import java.lang.Exception
 
 class FirebaseFunctionsRepository(
     private val deviceInfo: DeviceInfo,
     private val prefsRepository: SharedPrefsRepository
 ) {
 
+    /**
+     * Creates a new registration, saved to registrations collection.
+     */
     suspend fun registerEhrid() {
         val data = hashMapOf(
             "platform" to "android",
@@ -27,6 +32,9 @@ class FirebaseFunctionsRepository(
         prefsRepository.saveEhrid(ehrid)
     }
 
+    /**
+     * Returns data from collections covidDataIncrease and covidDataTotal for the given input date (if date is missing, TODAY is used)
+     */
     suspend fun getStats(date: String? = null): CovidStatsResponse {
         val data = hashMapOf(
             "date" to date
@@ -35,12 +43,47 @@ class FirebaseFunctionsRepository(
         return Gson().fromJson(covidStats.toString(), CovidStatsResponse::class.java)
     }
 
+    /**
+     * In the registrations collection, changes the value of lastNotificationStatus attribute to sent and the value of lastNotificationUpdatedAt attribute to CURRENT_TIMESTAMP for a given eHRID.
+     */
+    suspend fun registerNotification(ehrid: String) {
+        val data = hashMapOf(
+            "ehrid" to ehrid
+        )
+        callFunction("registerNotification", data) {
+            // TODO do some retry
+        }
+    }
+
+    /**
+     * Changes push token for eHRID.
+     */
+    suspend fun changePushToken(ehrid: String, pushRegistrationToken: String) {
+        val data = hashMapOf(
+            "ehrid" to ehrid,
+            "pushRegistrationToken" to pushRegistrationToken
+        )
+        callFunction("changePushToken", data) {
+            // TODO do some retry
+        }
+    }
+
+    /**
+     * Generic function for calling Firebase Function.
+     */
     private suspend fun callFunction(
         name: String,
-        data: Map<String, String?>? = null
+        data: Map<String, String?>? = null,
+        onFailure: ((Exception) -> Unit)? = null
     ): Map<String, String> {
         @Suppress("UNCHECKED_CAST")
         return Firebase.functions(FIREBASE_REGION).getHttpsCallable(name).call(data)
-            .await().data as Map<String, String>
+            .continueWith { task ->
+                (task.result?.data as HashMap<String, String>)
+            }
+            .addOnFailureListener { fail ->
+                L.e("Firebase Function " + name + " failed. Reason : " + fail.localizedMessage)
+                onFailure?.invoke(fail)
+            }.await()
     }
 }
