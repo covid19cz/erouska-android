acd7b77fcec941b9bfbfba87f1f4b88fa0cb1a82
Stepan Sonsky <stepansonsky@gmail.com>
2020-09-07 17:20:10 +0200

    moved risky exposure notification to broadcast receiver

--- app/src/main/kotlin/cz/covid19cz/erouska/exposurenotifications/worker/DownloadKeysWorker.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/exposurenotifications/worker/DownloadKeysWorker.kt
@@ -23,27 +23,15 @@ class DownloadKeysWorker(
         const val TAG = "DOWNLOAD_KEYS"
     }
 
-    private val prefs: SharedPrefsRepository by inject()
+
     private val exposureNotificationsRepository: ExposureNotificationsRepository by inject()
     private val serverRepository: ExposureServerRepository by inject()
-    private val firebaseFunctionsRepository: FirebaseFunctionsRepository by inject()
 
     override suspend fun doWork(): Result {
         L.i("Starting periodical key download")
         val files = serverRepository.downloadKeyExport()
         L.i("Extracted ${files.size} files")
         exposureNotificationsRepository.provideDiagnosisKeys(files)
-        checkExposure(context)
         return Result.success()
-    }
-
-    private suspend fun checkExposure(context: Context) {
-        val lastExposure = exposureNotificationsRepository.getLastRiskyExposure()?.daysSinceEpoch
-        val lastNotifiedExposure = prefs.getLastNotifiedExposure()
-        if (lastExposure != null && lastNotifiedExposure != 0 && lastExposure != lastNotifiedExposure) {
-            firebaseFunctionsRepository.registerNotification()
-            LocalNotificationsHelper.showRiskyExposureNotification(context)
-            prefs.setLastNotifiedExposure(lastExposure)
-        }
     }
 }
