f2849007856946d99bd34c5774c7c8dcaf7d2824
Tomáš Chlápek <tochlape@microsoft.com>
2020-09-12 16:56:32 +0200

    Recent exposure logic update

--- app/src/main/kotlin/cz/covid19cz/erouska/ui/exposure/RecentExposuresVM.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/ui/exposure/RecentExposuresVM.kt
@@ -3,26 +3,47 @@ package cz.covid19cz.erouska.ui.exposure
 import androidx.lifecycle.viewModelScope
 import arch.event.SingleLiveEvent
 import arch.viewmodel.BaseArchViewModel
+import cz.covid19cz.erouska.exposurenotifications.ExposureNotificationsRepository
+import cz.covid19cz.erouska.ext.daysSinceEpochToDateString
 import cz.covid19cz.erouska.ui.exposure.event.RecentExposuresEvent
+import cz.covid19cz.erouska.utils.L
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class RecentExposuresVM : BaseArchViewModel() {
+class RecentExposuresVM(private val exposureNotificationsRepo: ExposureNotificationsRepository) :
+    BaseArchViewModel() {
 
     val state = SingleLiveEvent<RecentExposuresEvent>()
 
-    fun loadExposures() {
-        viewModelScope.launch(Dispatchers.IO) {
-            // TODO Replace with loading of real exposure data, remove this mock
-            val mockData = listOf(
-                Exposure("28. prosince 2019"),
-                Exposure("20. března 2019"),
-                Exposure("14. května 2019"),
-                Exposure("5. srpna 2019")
-            )
+    fun loadExposures(demo: Boolean = false) {
+        if (!demo) {
+            viewModelScope.launch {
+                kotlin.runCatching {
+                    exposureNotificationsRepo.getDailySummaries()
+                }.onSuccess { dailySummaries ->
+                    if (dailySummaries.isNotEmpty()) {
+                        val exposureList = emptyList<Exposure>()
+                        for (dailySummary in dailySummaries) {
+                            exposureList.plus(Exposure(dailySummary.daysSinceEpoch.daysSinceEpochToDateString()))
+                        }
+                        state.postValue(RecentExposuresEvent.ExposuresLoadedEvent(exposureList))
+                    }
+                }.onFailure {
+                    L.e(it)
+                }
+            }
+        } else {
+            viewModelScope.launch(Dispatchers.IO) {
+                val mockData = listOf(
+                    Exposure("28. prosince 2019"),
+                    Exposure("20. března 2019"),
+                    Exposure("14. května 2019"),
+                    Exposure("5. srpna 2019")
+                )
 
-            state.postValue(RecentExposuresEvent.ExposuresLoadedEvent(mockData))
+                state.postValue(RecentExposuresEvent.ExposuresLoadedEvent(mockData))
+            }
         }
-    }
 
+    }
 }
