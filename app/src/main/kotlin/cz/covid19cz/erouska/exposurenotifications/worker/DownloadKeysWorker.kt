f6565e209d73f35dc96a106f043e7a226c56e602
David Vávra <david@vavra.me>
2020-09-14 19:59:12 +0200

    Update DownloadKeysWorker.kt

--- app/src/main/kotlin/cz/covid19cz/erouska/exposurenotifications/worker/DownloadKeysWorker.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/exposurenotifications/worker/DownloadKeysWorker.kt
@@ -1,16 +1,13 @@
 package cz.covid19cz.erouska.exposurenotifications.worker
 
 import android.content.Context
+import android.os.Bundle
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import cz.covid19cz.erouska.db.SharedPrefsRepository
+import com.google.firebase.analytics.FirebaseAnalytics
 import cz.covid19cz.erouska.exposurenotifications.ExposureNotificationsRepository
-import cz.covid19cz.erouska.exposurenotifications.LocalNotificationsHelper
 import cz.covid19cz.erouska.net.ExposureServerRepository
-import cz.covid19cz.erouska.net.FirebaseFunctionsRepository
 import cz.covid19cz.erouska.utils.L
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import org.koin.core.KoinComponent
 import org.koin.core.inject
 
@@ -23,15 +20,17 @@ class DownloadKeysWorker(
         const val TAG = "DOWNLOAD_KEYS"
     }
 
-
     private val exposureNotificationsRepository: ExposureNotificationsRepository by inject()
     private val serverRepository: ExposureServerRepository by inject()
+    private val analytics = FirebaseAnalytics.getInstance(context)
 
     override suspend fun doWork(): Result {
+        analytics.logEvent("key_export_download_started", Bundle())
         L.i("Starting periodical key download")
         val files = serverRepository.downloadKeyExport()
         L.i("Extracted ${files.size} files")
         exposureNotificationsRepository.provideDiagnosisKeys(files)
+        analytics.logEvent("key_export_download_finished", Bundle())
         return Result.success()
     }
 }
