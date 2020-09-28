398b36ba0bf6d01d5968393d21286fd2510306f1
Tomas Chlapek <tomas.chlapek@gmail.com>
2020-09-28 18:28:21 +0200

    Safe navigation update (#380)
    Every navigate() function wrapped with try-catch block.
    
    Co-authored-by: Tomáš Chlápek <tochlape@microsoft.com>

--- arch/src/main/java/arch/extensions/navExtensions.kt
+++ arch/src/main/java/arch/extensions/navExtensions.kt
@@ -1,18 +1,41 @@
 package arch.extensions
 
+import android.os.Bundle
+import androidx.annotation.IdRes
 import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
 import arch.event.NavigationEvent
 import arch.event.NavigationGraphEvent
 import arch.utils.safeLet
 import cz.stepansonsky.mvvm.BuildConfig
 
-fun NavController.navigate(navEvent: NavigationEvent) {
+fun NavController.safeNavigate(navEvent: NavigationEvent) {
     try {
         if (navEvent.resId != null) {
             navigate(navEvent.resId!!, navEvent.navArgs, navEvent.navOptions)
         } else if (navEvent.navDirections != null) {
             navigate(navEvent.navDirections!!, navEvent.navOptions)
         }
+    } catch (it: Throwable){
+        if (BuildConfig.DEBUG) {
+            it.printStackTrace()
+        }
+    }
+}
+
+fun NavController.safeNavigate(@IdRes resId: Int, args: Bundle? = null, navOptions: NavOptions? = null) {
+    try {
+        navigate(resId, args, navOptions)
+    } catch (it: Throwable){
+        if (BuildConfig.DEBUG) {
+            it.printStackTrace()
+        }
+    }
+}
+fun NavController.safeNavigate(directions: NavDirections, navOptions: NavOptions? = null) {
+    try {
+        navigate(directions, navOptions)
     } catch (it: Throwable){
         if (BuildConfig.DEBUG) {
             it.printStackTrace()
