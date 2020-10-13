8bbea67128b21f8f511fff5237f8e186a0a872d7
Tomáš Chlápek <tochlape@microsoft.com>
2020-10-13 08:48:59 +0200

    Custom search toolbar added "Scroll To Next" and "Scroll To Previous" search result buttons added

--- app/src/main/kotlin/cz/covid19cz/erouska/ui/help/HelpFragment.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/ui/help/HelpFragment.kt
@@ -3,21 +3,23 @@ package cz.covid19cz.erouska.ui.help
 import android.app.SearchManager
 import android.content.Context
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
+import android.view.*
 import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 import cz.covid19cz.erouska.AppConfig
 import cz.covid19cz.erouska.R
 import cz.covid19cz.erouska.databinding.FragmentHelpBinding
-import cz.covid19cz.erouska.ext.*
+import cz.covid19cz.erouska.ext.hide
+import cz.covid19cz.erouska.ext.show
+import cz.covid19cz.erouska.ext.showWeb
 import cz.covid19cz.erouska.ui.base.BaseFragment
 import cz.covid19cz.erouska.ui.help.event.HelpCommandEvent
 import cz.covid19cz.erouska.utils.CustomTabHelper
 import cz.covid19cz.erouska.utils.Markdown
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_help.*
+import kotlinx.android.synthetic.main.search_toolbar.*
+import java.util.regex.Pattern
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -27,6 +29,7 @@ class HelpFragment :
     @Inject
     internal lateinit var markdown: Markdown
     private var isFullscreen: Boolean = false
+
 
     @Inject
     internal lateinit var customTabHelper: CustomTabHelper
@@ -53,24 +56,36 @@ class HelpFragment :
     var lastMarkedIndex = 0
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.help, menu)
+        showSearchView()
 
         // Associate searchable configuration with the SearchView
         val searchManager = activity?.getSystemService(Context.SEARCH_SERVICE) as SearchManager
-        (menu.findItem(R.id.search).actionView as SearchView).apply {
+
+        activity?.toolbar_search_view?.apply {
             isIconifiedByDefault = true
+
+            setOnSearchClickListener {
+                menu.findItem(R.id.previous_search_result)?.isVisible = true
+                menu.findItem(R.id.next_search_result)?.isVisible = true
+                menu.findItem(R.id.nav_about)?.isVisible = false
+                (activity as? AppCompatActivity)?.supportActionBar?.setDisplayShowTitleEnabled(false)
+            }
+
+            setOnCloseListener {
+                menu.findItem(R.id.previous_search_result)?.isVisible = false
+                menu.findItem(R.id.next_search_result)?.isVisible = false
+                menu.findItem(R.id.nav_about)?.isVisible = true
+                (activity as? AppCompatActivity)?.supportActionBar?.setDisplayShowTitleEnabled(true)
+                false
+            }
+
+            setSearchControlButtons(menu, false)
+
             setSearchableInfo(searchManager.getSearchableInfo(activity?.componentName))
             setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     query?.let {
-                        if (query.isNotBlank() && AppConfig.helpMarkdown.contains(query)) {
-                            lastMarkedIndex = help_desc.text.toString().indexOf(
-                                query,
-                                lastMarkedIndex + query.length
-                            )
-                            val lineNumber: Int = help_desc.layout.getLineForOffset(lastMarkedIndex)
-
-                            help_scroll.scrollTo(0, help_desc.layout.getLineTop(lineNumber))
-                        }
+                        scrollToNextResult(query)
                         return true
                     }
                     return false
@@ -83,23 +98,46 @@ class HelpFragment :
                                 ignoreCase = true
                             )
                         ) {
+                            lastMarkedIndex = 0
+                            setSearchControlButtons(menu, true)
+
+                            val pattern = query.trim()
+                            val r =
+                                Pattern.compile(pattern, Pattern.CASE_INSENSITIVE)
+                            var result = AppConfig.helpMarkdown
+                            val m = r.matcher(result)
+                            val replaceList = arrayListOf<String>()
+                            while (m.find()) {
+                                replaceList.add(result.substring(m.start(0), m.end(0)))
+                            }
+
+                            showSnackBar("${replaceList.size} search results")
+
+                            for (replaceString in replaceList.distinct()) {
+                                result = result.replace(replaceString, "**${replaceString}**")
+                            }
+
                             lastMarkedIndex =
-                                AppConfig.helpMarkdown.indexOf(query, ignoreCase = true)
-                            val lineNumber: Int = help_desc.layout.getLineForOffset(lastMarkedIndex)
-                            val highlighted = "```${query.trim()}```"
-                            var fullText = AppConfig.helpMarkdown.replace(
-                                query.trim(),
-                                highlighted,
-                                ignoreCase = true
-                            )
-                            markdown.show(help_desc, fullText)
+                                help_desc.text.toString().indexOf(query, ignoreCase = true)
+                            val lineNumber: Int =
+                                help_desc.layout.getLineForOffset(lastMarkedIndex)
+
+                            markdown.show(help_desc, result)
 
                             if (lineNumber >= 0) {
                                 help_scroll.scrollTo(0, help_desc.layout.getLineTop(lineNumber))
                             }
+
+
                         } else {
                             markdown.show(help_desc, AppConfig.helpMarkdown)
-                            help_scroll.scrollTo(0, 0)
+//                            help_scroll.scrollTo(0, 0)
+
+                            // show SnackBar only if user made some search query
+                            if (query.isNotBlank()) {
+                                showSnackBar("No search result")
+                            }
+                            setSearchControlButtons(menu, false)
                         }
                     }
                     return true
@@ -107,6 +145,87 @@ class HelpFragment :
             })
         }
         super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        collapseAndHideSearchView()
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    fun goBack() {
+        navController().navigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.nav_about -> {
+                navigate(HelpFragmentDirections.actionNavHelpToNavAbout(isFullscreen))
+                true
+            }
+            R.id.previous_search_result -> {
+                scrollToPreviousResult(activity?.toolbar_search_view?.query.toString())
+                true
+            }
+            R.id.next_search_result -> {
+                scrollToNextResult(activity?.toolbar_search_view?.query.toString())
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    private fun showSearchView() {
+        activity?.toolbar_search_view?.show()
+    }
+
+    private fun collapseAndHideSearchView() {
+        activity?.toolbar_search_view?.apply {
+            setQuery("", false)
+            isIconified = true
+            hide()
+        }
+        (activity as? AppCompatActivity)?.supportActionBar?.setDisplayShowTitleEnabled(true)
+    }
+
+    /**
+     * Scroll to previous search result
+     */
+    private fun scrollToPreviousResult(query: String) {
+        if (query.isNotBlank() && AppConfig.helpMarkdown.contains(query, ignoreCase = true)) {
+            if (lastMarkedIndex == -1) {
+                lastMarkedIndex = help_desc.text.toString().length
+            }
+            lastMarkedIndex = help_desc.text.toString().substring(0, lastMarkedIndex).lastIndexOf(
+                query, ignoreCase = true
+            )
+            val lineNumber: Int = help_desc.layout.getLineForOffset(lastMarkedIndex)
+            help_scroll.scrollTo(0, help_desc.layout.getLineTop(lineNumber))
+        }
+    }
+
+    /**
+     * Scroll to next search result
+     */
+    private fun scrollToNextResult(query: String) {
+        if (query.isNotBlank() && AppConfig.helpMarkdown.contains(query, ignoreCase = true)) {
+            lastMarkedIndex = help_desc.text.toString().indexOf(
+                query,
+                lastMarkedIndex + query.length,
+                ignoreCase = true
+            )
+            val lineNumber: Int = help_desc.layout.getLineForOffset(lastMarkedIndex)
+            help_scroll.scrollTo(0, help_desc.layout.getLineTop(lineNumber))
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -128,19 +247,10 @@ class HelpFragment :
         }
     }
 
-    fun goBack() {
-        navController().navigateUp()
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.nav_about -> {
-                navigate(HelpFragmentDirections.actionNavHelpToNavAbout(isFullscreen))
-                true
-            }
-            else -> {
-                super.onOptionsItemSelected(item)
-            }
-        }
+    private fun setSearchControlButtons(menu: Menu, shouldEnable: Boolean) {
+        menu.findItem(R.id.previous_search_result)?.icon?.alpha = if (shouldEnable) 255 else 75
+        menu.findItem(R.id.next_search_result)?.icon?.alpha = if (shouldEnable) 255 else 75
+        menu.findItem(R.id.previous_search_result)?.isEnabled = shouldEnable
+        menu.findItem(R.id.next_search_result)?.isEnabled = shouldEnable
     }
 }
--- app/src/main/kotlin/cz/covid19cz/erouska/utils/Markdown.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/utils/Markdown.kt
@@ -2,18 +2,20 @@ package cz.covid19cz.erouska.utils
 
 import android.content.Context
 import android.graphics.Color
+import android.text.style.BackgroundColorSpan
 import android.widget.TextView
-import androidx.annotation.NonNull
 import dagger.hilt.android.qualifiers.ApplicationContext
 import io.noties.markwon.AbstractMarkwonPlugin
 import io.noties.markwon.Markwon
-import io.noties.markwon.core.MarkwonTheme
+import io.noties.markwon.MarkwonSpansFactory
 import io.noties.markwon.html.HtmlPlugin
 import io.noties.markwon.image.glide.GlideImagesPlugin
 import org.commonmark.ext.autolink.AutolinkExtension
+import org.commonmark.node.StrongEmphasis
 import org.commonmark.parser.Parser
 import javax.inject.Inject
 import javax.inject.Singleton
+
 
 @Singleton
 class Markdown @Inject constructor(@ApplicationContext val context: Context) {
@@ -27,13 +29,13 @@ class Markdown @Inject constructor(@ApplicationContext val context: Context) {
                 }
             })
             .usePlugin(object : AbstractMarkwonPlugin() {
-                override fun configureTheme(@NonNull builder: MarkwonTheme.Builder) {
+                override fun configureSpansFactory(builder: MarkwonSpansFactory.Builder) {
                     builder
-                        .codeBlockMargin(0)
-                        .codeTextColor(Color.BLACK)
-                        .codeBlockTextColor(Color.BLACK)
-                        .codeBackgroundColor(Color.YELLOW)
-                        .codeBlockBackgroundColor(Color.YELLOW)
+                        .setFactory(
+                            StrongEmphasis::class.java
+                        ) { configuration, props -> arrayOf(
+                            BackgroundColorSpan(Color.GREEN)
+                        ) }
                 }
             })
             .build()
