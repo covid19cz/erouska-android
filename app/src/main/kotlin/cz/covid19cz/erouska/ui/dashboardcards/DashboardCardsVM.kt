d8de8294f342cf81dbdc7a89a7d65ecc6decbe4e
Tomáš Chlápek <tochlape@microsoft.com>
2020-10-17 22:50:36 +0200

    Dashboard redesign
    Custom DashboardCardView created.
    DashboardCards Testing fragment created for card demonstration.
    Icons added.
    Strings updated.
    UI prepared for logic integration.

---
+++ app/src/main/kotlin/cz/covid19cz/erouska/ui/dashboardcards/DashboardCardsVM.kt
@@ -0,0 +1,8 @@
+package cz.covid19cz.erouska.ui.dashboardcards
+
+import androidx.hilt.lifecycle.ViewModelInject
+import cz.covid19cz.erouska.ui.base.BaseVM
+
+class DashboardCardsVM @ViewModelInject constructor() : BaseVM() {
+
+}
