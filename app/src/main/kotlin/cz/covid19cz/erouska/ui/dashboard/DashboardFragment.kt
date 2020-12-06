c076ef639cbd9a28b68975dfd9cead80dcd7c5a6
Vanda Cabanova <cabanovav@gmail.com>
2020-12-06 18:50:11 +0100

    AND-174 Added an analytics event for SHARE APP button

--- app/src/main/kotlin/cz/covid19cz/erouska/ui/dashboard/DashboardFragment.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/ui/dashboard/DashboardFragment.kt
@@ -27,6 +27,7 @@ import cz.covid19cz.erouska.ui.dashboard.event.DashboardCommandEvent
 import cz.covid19cz.erouska.ui.dashboard.event.GmsApiErrorEvent
 import cz.covid19cz.erouska.ui.exposure.event.ExposuresCommandEvent
 import cz.covid19cz.erouska.ui.main.MainVM
+import cz.covid19cz.erouska.utils.Analytics
 import cz.covid19cz.erouska.utils.showOrHide
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_dashboard.*
@@ -41,6 +42,8 @@ class DashboardFragment : BaseFragment<FragmentDashboardPlusBinding, DashboardVM
 
     companion object {
         private const val SCREEN_NAME = "Dashboard"
+
+        private const val ANALYTICS_KEY_SHARE_APP = "click_share_app"
     }
 
     private val mainViewModel: MainVM by activityViewModels()
@@ -203,6 +206,7 @@ class DashboardFragment : BaseFragment<FragmentDashboardPlusBinding, DashboardVM
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_share -> {
+                Analytics.logEvent(requireContext(), ANALYTICS_KEY_SHARE_APP)
                 requireContext().shareApp()
                 true
             }
