d78be802a2a7bc3c024ccd3a9b7cb050b6d54f6a
Tomáš Chlápek <tochlape@microsoft.com>
2021-03-12 13:35:18 +0100

    String update. Turn on EFGS automatically on EFGS screen.

--- app/src/main/kotlin/cz/covid19cz/erouska/ui/update/efgs/EfgsUpdateFragment.kt
+++ app/src/main/kotlin/cz/covid19cz/erouska/ui/update/efgs/EfgsUpdateFragment.kt
@@ -63,6 +63,9 @@ class EfgsUpdateFragment : BaseFragment<FragmentEfgsUpdateBinding, EfgsUpdateVM>
             }
         }
 
+        // turn on EFGS automatically on this screen
+        viewModel.sharedPrefsRepository.setTraveller(true)
+
         legacy_update_checkbox.isChecked = viewModel.sharedPrefsRepository.isTraveller()
         legacy_update_checkbox.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
